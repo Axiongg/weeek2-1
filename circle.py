7a485bbcb8e0a117183cc410703fc49311aac498
Axiongg <161007911+Axiongg@users.noreply.github.com>
2024-03-05 16:12:49 +0800

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+π=3.141592653589
+r=float(input("請輸入半徑="))
+a=2*π*r
+print("圓周長=")
+print(a)
+b=π*r*r
+print("園面積=")
+print(b)
